7c163e3729d61d35c9a770e265f90f1891118138
simplyana08 <ana.t08@yahoo.com>
2026-03-29 01:36:54 +0100

    mijn project tot nu

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sushi Restaurant</title>
+</head>
+<body>
+    <h1>Welcome to Sushi Restaurant 🍣</h1>
+
+    <nav>
+        <a href="/">Home</a> |
+        <a href="/menu">Menu</a> |
+        <a href="/contact">Contact</a> |
+        <a href="/login">Login</a>
+    </nav>
+
+    <p>This is our homepage.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\PageController;
+aaaa
+Route::get('/', [PageController::class, 'home'])->name('home');
